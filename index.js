3ad26867745376a9c9fead678c03816c830e93bf
Rahat92 <khrahat92@gmail.com>
2024-10-27 12:53:49 +0600

    update form

--- index.js
+++ index.js
@@ -1,99 +1,3 @@
-// const express = require('express');
-// const { PDFDocument, rgb } = require('pdf-lib'); // Import pdf-lib
-
-// const app = express();
-
-// app.get('/generate-pdf', async (req, res) => {
-//   try {
-//     // Create a new PDF document
-//     const pdfDoc = await PDFDocument.create();
-
-//     // Add a blank page to the document
-//     const page = pdfDoc.addPage([600, 400]);
-
-//     // Draw text on the page
-//     const { width, height } = page.getSize();
-//     page.drawText('Hello, this is your PDF!', {
-//       x: 50,
-//       y: height - 50,
-//       size: 24,
-//       color: rgb(0, 0, 0.75),
-//     });
-
-//     // Serialize the PDF document to bytes (Uint8Array)
-//     const pdfBytes = await pdfDoc.save();
-
-//     // Set response headers for downloading PDF
-//     res.set({
-//       'Content-Type': 'application/pdf',
-//       'Content-Disposition': 'attachment; filename=generated.pdf',
-//     });
-
-//     // Send the generated PDF
-//     res.send(Buffer.from(pdfBytes));
-//   } catch (error) {
-//     console.error('Error generating PDF:', error);
-//     res.status(500).send('Error generating PDF');
-//   }
-// });
-
-// // Start the server
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
-
-
-
-// const express = require('express');
-// const { PDFDocument, rgb } = require('pdf-lib'); // Import pdf-lib
-
-// const app = express();
-
-// app.get('/preview-pdf', async (req, res) => {
-//   try {
-//     // Create a new PDF document
-//     const pdfDoc = await PDFDocument.create();
-
-//     // Add a blank page to the document
-//     const page = pdfDoc.addPage([600, 400]);
-
-//     // Draw text on the page
-//     const { width, height } = page.getSize();
-//     page.drawText('Hello, this is your PDF preview!', {
-//       x: 50,
-//       y: height - 50,
-//       size: 24,
-//       color: rgb(0, 0, 0.75),
-//     });
-
-//     // Serialize the PDF document to bytes (Uint8Array)
-//     const pdfBytes = await pdfDoc.save();
-
-//     // Set response headers for inline preview in the browser
-//     const fileName = 'example-preview.pdf'; // You can dynamically set this based on request data
-//     res.set({
-//       'Content-Type': 'application/pdf',
-//       'Content-Disposition': `inline; filename=${fileName}`, // Display in browser
-//     });
-
-//     // Send the generated PDF
-//     res.send(Buffer.from(pdfBytes));
-//   } catch (error) {
-//     console.error('Error generating PDF:', error);
-//     res.status(500).send('Error generating PDF');
-//   }
-// });
-
-// // Start the server
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
-
-
-
-
 const express = require('express');
 const axios = require('axios')
 const regeneratorRuntime = require("regenerator-runtime");
@@ -101,7 +5,7 @@ const regeneratorRuntime = require("regenerator-runtime");
 const cors = require('cors')
 const { PDFDocument, rgb, setCharacterSpacing } = require('pdf-lib'); // Import pdf-lib
 const fontKit = require('@pdf-lib/fontkit')
-const fs = require('fs'); // For reading the existing PDF file
+const fs = require('fs');
 const path = require('path');
 const app = express();
 app.use(express.json())
@@ -109,10 +13,6 @@ app.use(cors({ origin: '*' }))
 
 app.post('/modify-pdf', async (req, res) => {
   console.log(req.body)
-  // fetch(req.body.clientPhoto).then(res => {
-  //   fs.writeFile('client-image')
-  // })
-  // downloadImage(req.body.clientPhoto, 'image.png')
   try {
     // Load the existing PDF file from the file system
     const existingPdfBytes = fs.readFileSync(__dirname + "/BO_Account_Open_Form.pdf"); // Update with your file path
@@ -125,40 +25,62 @@ app.post('/modify-pdf', async (req, res) => {
     const firstPage = pages[0]; // Modify this page or loop through other pages
     const { width, height } = firstPage.getSize();
     const secondPage = pages[1]; // Modify this page or loop through other pages
-    const { width:secondPageWidth, height:secondPageHeight } = secondPage.getSize();
+    const { width: secondPageWidth, height: secondPageHeight } = secondPage.getSize();
     const fetchImage = async (url) => {
       const response = await axios.get(url, { responseType: 'arraybuffer' });
       return response.data;
     };
-    const imageBytes = await fetchImage(req.body.clientPhoto);
-    const signatureBytes = await fetchImage(req.body.clientSignature)
-    let image;
+    if (req.body.clientPhoto) {
+      const imageBytes = await fetchImage(req.body.clientPhoto);
 
-    try {
-      image = await pdfDoc.embedPng(imageBytes); // Try to embed as PNG
-    } catch (error) {
-      image = await pdfDoc.embedJpg(imageBytes); // Fallback to JPG
-    }
-    let signature;
+      let image;
 
-    try {
-      signature = await pdfDoc.embedPng(signatureBytes); // Try to embed as PNG
-    } catch (error) {
-      signature = await pdfDoc.embedJpg(signatureBytes); // Fallback to JPG
+      try {
+        image = await pdfDoc.embedPng(imageBytes); // Try to embed as PNG
+      } catch (error) {
+        image = await pdfDoc.embedJpg(imageBytes); // Fallback to JPG
+      }
+      secondPage.drawImage(image, {
+        x: 92, // Position the image
+        y: secondPageHeight - 460.5, // Position from the top
+        width: 102,
+        height: 105
+      })
     }
-    secondPage.drawImage(image, {
-      x: 92, // Position the image
-      y: secondPageHeight-460.5, // Position from the top
-      width: 102,
-      height: 105
-    })
-    secondPage.drawImage(signature, {
-      x: 400, // Position the image
-      y: secondPageHeight-649.5, // Position from the top
-      width: 102,
-      height: 26
-    })
-    
+
+    if (req.body.clientSignature) {
+      const signatureBytes = await fetchImage(req.body.clientSignature)
+      let signature;
+      try {
+        signature = await pdfDoc.embedPng(signatureBytes); // Try to embed as PNG
+      } catch (error) {
+        signature = await pdfDoc.embedJpg(signatureBytes); // Fallback to JPG
+      }
+      secondPage.drawImage(signature, {
+        x: 400, // Position the image
+        y: secondPageHeight - 649.5, // Position from the top
+        width: 102,
+        height: 26
+      })
+    }
+
+
+    if (req.body.jointApplicantPhoto) {
+      const jointApplicantPhotoBytes = await fetchImage(req.body.jointApplicantPhoto)
+      let jointApplicantPhoto;
+      try {
+        jointApplicantPhoto = await pdfDoc.embedPng(jointApplicantPhotoBytes); // Try to embed as PNG
+      } catch (error) {
+        jointApplicantPhoto = await pdfDoc.embedJpg(jointApplicantPhotoBytes); // Fallback to JPG
+      }
+      secondPage.drawImage(jointApplicantPhoto, {
+        x: 263, // Position the image
+        y: secondPageHeight - 460.5, // Position from the top
+        width: 102,
+        height: 105
+      })
+    }
+
     const fontBytes = fs.readFileSync(__dirname + "/fonts/NotoSansSymbols-Regular.ttf");
     const bengliFontBytes = fs.readFileSync(__dirname + "/fonts/Shonar.ttf");
     const customFont = await pdfDoc.embedFont(fontBytes);
@@ -208,45 +130,78 @@ app.post('/modify-pdf', async (req, res) => {
         });
       }
     }
-  
+
     // account holder name
-    generateAndPlaceText(firstPage, 'name', req.body.clientName, 51, 307.8, 10, { r: 0, g: 0, b: 0 }, 4.8, '');
+    if(req.body.clientName){
+      generateAndPlaceText(firstPage, 'name', req.body.clientName, 51, 307.8, 10, { r: 0, g: 0, b: 0 }, 4.8, '');
+      generateAndPlaceText(secondPage, 'name', req.body.clientName, 150, 640.8, 12, { r: 0, g: 0, b: 0 }, 0, '');
+    }
+
+    if(req.body.jointApplicantName){
+      generateAndPlaceText(secondPage, 'name', req.body.jointApplicantName, 150, 665.8, 12, { r: 0, g: 0, b: 0 }, 0, '');
+      generateAndPlaceText(firstPage, 'name', req.body.jointApplicantName, 153, 805.8, 10, { r: 0, g: 0, b: 0 }, 0, '');
+    }
+    
 
     // Date of birth
-    generateAndPlaceText(firstPage, 'name', req.body.clientDateOfBirth, 445, 664.5, 9, { r: 0, g: 0, b: 0 }, 5.1, '');
+    if(req.body.clientDateOfBirth){
+      generateAndPlaceText(firstPage, 'name', req.body.clientDateOfBirth, 445, 664.5, 9, { r: 0, g: 0, b: 0 }, 5.1, '');
+    }
 
     // occupation
-    generateAndPlaceText(firstPage, 'name', req.body.clientOccupation, 330, 345, 10, { r: 0, g: 0, b: 0 }, undefined, '');
+    if(req.body.clientOccupation){
+      generateAndPlaceText(firstPage, 'name', req.body.clientOccupation, 330, 345, 10, { r: 0, g: 0, b: 0 }, undefined, '');
+    }
 
     // father/husband name
-    generateAndPlaceText(firstPage, 'name', req.body.clientGuardian, 145, 363, 10, { r: 0, g: 0, b: 0 }, undefined, '');
+    if(req.body.clientGuardian){
+      generateAndPlaceText(firstPage, 'name', req.body.clientGuardian, 145, 363, 10, { r: 0, g: 0, b: 0 }, undefined, '');
+    }
 
     // mother name
-    generateAndPlaceText(firstPage, 'name', req.body.clientMother, 110, 382, 10, { r: 0, g: 0, b: 0 }, undefined, '');
+    if(req.body.clientMother){
+      generateAndPlaceText(firstPage, 'name', req.body.clientMother, 110, 382, 10, { r: 0, g: 0, b: 0 }, undefined, '');
+    }
 
     // address
-    generateAndPlaceText(firstPage, 'name', req.body.clientAddress, 90, 430, 10, { r: 0, g: 0, b: 0 }, 0, '')
+    if(req.body.clientAddress){
+      generateAndPlaceText(firstPage, 'name', req.body.clientAddress, 90, 430, 10, { r: 0, g: 0, b: 0 }, 0, '')
+    }
 
     // city
-    generateAndPlaceText(firstPage, 'name', req.body.clientCity, 70, 445, 10, { r: 0, g: 0, b: 0 }, 0, '')
+    if(req.body.clientCity){
+      generateAndPlaceText(firstPage, 'name', req.body.clientCity, 70, 445, 10, { r: 0, g: 0, b: 0 }, 0, '')
+    }
 
     // postal code
-    generateAndPlaceText(firstPage, 'name', req.body.clientPostalCode, 195, 445, 10, { r: 0, g: 0, b: 0 }, 0, '')
+    if(req.body.clientPostalCode){
+      generateAndPlaceText(firstPage, 'name', req.body.clientPostalCode, 195, 445, 10, { r: 0, g: 0, b: 0 }, 0, '')
+    }
 
     // country
-    generateAndPlaceText(firstPage, 'name', req.body.clientCountry, 375, 445, 10, { r: 0, g: 0, b: 0 }, 0, '')
+    if(req.body.clientCountry){
+      generateAndPlaceText(firstPage, 'name', req.body.clientCountry, 375, 445, 10, { r: 0, g: 0, b: 0 }, 0, '')
+    }
 
     // mobile
-    generateAndPlaceText(firstPage, 'name', req.body.clientMobileNumber, 98, 465, 10, { r: 0, g: 0, b: 0 }, 0, '')
+    if(req.body.clientMobileNumber){
+      generateAndPlaceText(firstPage, 'name', req.body.clientMobileNumber, 96, 465, 10, { r: 0, g: 0, b: 0 }, 0, '')
+    }
 
     // email
-    generateAndPlaceText(firstPage, 'name', req.body.clientEmail, 285, 465, 10, { r: 0, g: 0, b: 0 }, 0, '')
+    if(req.body.clientEmail){
+      generateAndPlaceText(firstPage, 'name', req.body.clientEmail, 285, 465, 10, { r: 0, g: 0, b: 0 }, 0, '')
+    }
 
     // nid
-    generateAndPlaceText(firstPage, 'name', req.body.clientNid, 155, 720, 10, { r: 0, g: 0, b: 0 }, 0, '')
+    if(req.body.clientNid){
+      generateAndPlaceText(firstPage, 'name', req.body.clientNid, 155, 720, 10, { r: 0, g: 0, b: 0 }, 0, '')
+    }
 
     // bank name
-    generateAndPlaceText(firstPage, 'name', 'Commercial Bank Of Ceylon PLC', 90, 575, 10, { r: 0, g: 0, b: 0 }, 0, '')
+    if(req.body.clientBankName){
+      generateAndPlaceText(firstPage, 'name', req.body.clientBankName, 90, 575, 10, { r: 0, g: 0, b: 0 }, 0, '')
+    }
 
     // branch name
     generateAndPlaceText(firstPage, 'name', 'Progoti Sharani SME', 310, 575, 10, { r: 0, g: 0, b: 0 }, 0, '')
@@ -257,36 +212,17 @@ app.post('/modify-pdf', async (req, res) => {
     // bank account number
     generateAndPlaceText(firstPage, 'name', '8813002586', 335, 555, 10, { r: 0, g: 0, b: 0 }, 0, '')
 
-    // firstPage.drawText('\u2713', {
-    //   // x: 127,   // X position
-    //   // x: 188,
-    //   x: 246,
-    //   y: height - 142,   // Y position
-    //   font: customFont,
-    //   size: 20, // Adjust size if needed
-    //   color: rgb(0, 0, 0), // Green color for the tick mark
-    // });
-    // generateAndPlaceText(firstPage, 'tickMark', '\u2713', 246, 142, 30, { r: 0, g: 0, b: 0 }, undefined, '')
-    // firstPage.drawText('\u2713', {
-    //   // x:393,
-    //   // x:455,
-    //   x: 530,
-    //   y: height - 142,   // Y position
-    //   font: customFont,
-    //   size: 20, // Adjust size if needed
-    //   color: rgb(0, 0, 0), // Green color for the tick mark
-    // });
-
     // bo type
+    generateAndPlaceText(firstPage, 'tickMark', '\u2713', 127, 142, 30, { r: 0, g: 0, b: 0 }, undefined, '')
     generateAndPlaceText(firstPage, 'tickMark', '\u2713', req.body.boType === 'single' ? 393 : req.body.boType === 'joint' ? 530 : 455, 142, 30, { r: 0, g: 0, b: 0 }, undefined, '')
 
     // gender
-    const male = false;
     generateAndPlaceText(firstPage, 'tickMark', '\u2713', req.body.clientGender === 'Male' ? 166 : 212, 340, 30, { r: 0, g: 0, b: 0 }, undefined, '')
 
     // Resident
     const resident = false;
     generateAndPlaceText(firstPage, 'tickMark', '\u2713', resident ? 125 : 188, 659, 30, { r: 0, g: 0, b: 0 }, undefined, '')
+
     // Nationallity
     generateAndPlaceText(firstPage, 'name', req.body.clientNationality, 250, 659, 10, { r: 0, g: 0, b: 0 }, 0, '')
 
@@ -314,80 +250,3 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-
-// // create multiple page
-
-
-// const express = require('express');
-// const { PDFDocument, rgb } = require('pdf-lib');
-
-// const app = express();
-
-// app.get('/multi-page-pdf', async (req, res) => {
-//   try {
-//     // Create a new PDF document
-//     const pdfDoc = await PDFDocument.create();
-
-//     // Add the first page
-//     const page1 = pdfDoc.addPage([600, 400]);
-//     page1.drawText('This is page 1!', {
-//       x: 50,
-//       y: 10,
-//       size: 24,
-//       color: rgb(0.2, 0.84, 0.67),
-//     });
-//     page1.drawText('Second line in page 1', {
-//       x: 50,
-//       y: 40,
-//       size: 24,
-//       color: rgb(0.2, 0.84, 0.67),
-//     });
-//     page1.drawText('Third line in page 1', {
-//       x: 50,
-//       y: 70,
-//       size: 24,
-//       color: rgb(0.2, 0.84, 0.67),
-//     });
-
-//     // Add a second page
-//     const page2 = pdfDoc.addPage([600, 400]);
-//     page2.drawText('Welcome to page 2!', {
-//       x: 50,
-//       y: 350,
-//       size: 24,
-//       color: rgb(0.1, 0.1, 0.75),
-//     });
-
-//     // Add a third page
-//     const page3 = pdfDoc.addPage([600, 400]);
-//     page3.drawText('This is the last page (Page 3)!', {
-//       x: 50,
-//       y: 350,
-//       size: 24,
-//       color: rgb(1, 0, 0),
-//     });
-
-//     // Serialize the PDF document to bytes
-//     const pdfBytes = await pdfDoc.save();
-
-//     // Set response headers for inline preview in the browser
-//     const fileName = 'multi-page-preview.pdf';
-//     res.set({
-//       'Content-Type': 'application/pdf',
-//       'Content-Disposition': `inline; filename=${fileName}`, // Display in browser
-//     });
-
-//     // Send the generated PDF
-//     res.send(Buffer.from(pdfBytes));
-//   } catch (error) {
-//     console.error('Error generating PDF:', error);
-//     res.status(500).send('Error generating PDF');
-//   }
-// });
-
-// // Start the server
-// const PORT = process.env.PORT || 3001;
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
