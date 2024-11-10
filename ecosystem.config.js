f960724a28502e259f3534c4088cd666cd210387
Rahat92 <khrahat92@gmail.com>
2024-11-10 14:40:53 +0600

    generate word file

---
+++ ecosystem.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  apps: [{
+    name: "Bo-form-backend",
+    script: "./index.js"
+  }]
+}
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios')
-const regeneratorRuntime = require("regenerator-runtime");
+const officegen = require('officegen');
+const { AlignmentType, Document, Footer, Header, HeadingLevel, Packer, Paragraph, TextRun, UnderlineType, Table, TableCell, TableRow, addLineBreak } = require('docx');
 
 const cors = require('cors')
 const { PDFDocument, rgb, setCharacterSpacing } = require('pdf-lib'); // Import pdf-lib
@@ -9,10 +10,128 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 app.use(express.json())
-app.use(cors({ origin: '*' }))
-
+app.use(cors({ origin: ["http://157.142.6.2:8082", "http://localhost:3000", "http://157.142.6.2:8080", "http://localhost:8080"] }))
 app.post('/modify-pdf', async (req, res) => {
+
+
+
+  let docx = officegen('docx')
+
+  // Officegen calling this function after finishing to generate the docx document:
+  docx.on('finalize', function (written) {
+    console.log(
+      'Finish to create a Microsoft Word document.'
+    )
+  })
+
+  // Officegen calling this function to report errors:
+  docx.on('error', function (err) {
+    console.log(err)
+  })
+
+  // Create a new paragraph:
+  let pObj = docx.createP()
+
+  Object.keys(req.body.fields).forEach((item, index) => {
+    pObj.addText(Object.keys(req.body.fields)[index], {
+      bold: true,
+      underline: true
+    })
+    pObj.addLineBreak()
+    pObj.addText(Object.values(req.body.fields)[index])
+    pObj = docx.createP()
+  })
+
+
+  // // pObj.addText(' and back color.', { color: '00ffff', back: '000088' })
+
+  
+
+  // pObj.addText('Since ')
+  // pObj.addText('officegen 0.2.12', {
+  //   back: '00ffff',
+  //   shdType: 'pct12',
+  //   shdColor: 'ff0000'
+  // }) // Use pattern in the background.
+  // pObj.addText(' you can do ')
+  // pObj.addText('more cool ', { highlight: true }) // Highlight!
+  // pObj.addText('stuff!', { highlight: 'darkGreen' }) // Different highlight color.
+
+  // pObj = docx.createP()
+
+  // pObj.addText('Even add ')
+  // pObj.addText('external link', { link: 'https://github.com' })
+  // pObj.addText('!')
+
+  // pObj = docx.createP()
+
+  // pObj.addText('Bold + underline', { bold: true, underline: true })
+
+  // pObj = docx.createP({ align: 'center' })
+
+  // pObj.addText('Center this text', {
+  //   border: 'dotted',
+  //   borderSize: 12,
+  //   borderColor: '88CCFF'
+  // })
+
+  // pObj = docx.createP()
+  // pObj.options.align = 'right'
+
+  // pObj.addText('Align this text to the right.')
+
+  // pObj = docx.createP()
+
+  // pObj.addText('Those two lines are in the same paragraph,')
+  // pObj.addLineBreak()
+  // pObj.addText('but they are separated by a line break.')
+
+  // docx.putPageBreak()
+
+  // pObj = docx.createP()
+
+  // pObj.addText('Fonts face only.', { font_face: 'Arial' })
+  // pObj.addText(' Fonts face and size.', { font_face: 'Arial', font_size: 40 })
+
+  // docx.putPageBreak()
+
+  // pObj = docx.createP()
+
+  // // We can even add images:
+  // pObj.addImage('images/M0123.png')
+
+  // Let's generate the Word document into a file:
+
+  let out = fs.createWriteStream(`C:\\Users\\ASUS\\Desktop\\BO\\${folderName}`)
+
+  out.on('error', function (err) {
+    console.log(err)
+  })
+
+  // Async call to generate the output file:
+  docx.generate(out)
+
+
+  const folderName = req.body.clientId;
+  const getFileExtension = (fileName) => {
+    const fileExtension = fileName.split('.')[fileName.split('.').length - 1];
+    return fileExtension;
+  }
   console.log(req.body)
+  if (!fs.existsSync(`C:\\Users\\ASUS\\Desktop\\BO\\${folderName}`)) {
+    fs.mkdirSync(path.join('C:\\Users\\ASUS\\Desktop\\BO', folderName), (err) => {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log('Folder is written')
+      }
+    })
+  } else {
+    return res.status(400).json({
+      status: 'Fail',
+      message: 'Already exist a folder with same name'
+    })
+  }
   try {
     // Load the existing PDF file from the file system
     const existingPdfBytes = fs.readFileSync(__dirname + "/BO_Account_Open_Form.pdf"); // Update with your file path
@@ -32,17 +151,26 @@ app.post('/modify-pdf', async (req, res) => {
     };
     if (req.body.clientPhoto) {
       const imageBytes = await fetchImage(req.body.clientPhoto);
-
+      fs.writeFile(`C:\\Users\\ASUS\\Desktop\\BO\\${folderName}/${folderName}-photo.jpg`, imageBytes, err => {
+        if (err) {
+          console.log(err)
+        }
+      })
       let image;
-
       try {
-        image = await pdfDoc.embedPng(imageBytes); // Try to embed as PNG
+        image = await pdfDoc.embedPng(imageBytes);
       } catch (error) {
-        image = await pdfDoc.embedJpg(imageBytes); // Fallback to JPG
+        console.error('Error embedding PNG:', error);
+        try {
+          image = await pdfDoc.embedJpg(imageBytes);
+        } catch (error) {
+          console.error('Error embedding JPG:', error);
+          throw new Error('Failed to embed image as either PNG or JPG.');
+        }
       }
       secondPage.drawImage(image, {
-        x: 92, // Position the image
-        y: secondPageHeight - 460.5, // Position from the top
+        x: 92,
+        y: secondPageHeight - 460.5,
         width: 102,
         height: 105
       })
@@ -50,6 +178,11 @@ app.post('/modify-pdf', async (req, res) => {
 
     if (req.body.clientSignature) {
       const signatureBytes = await fetchImage(req.body.clientSignature)
+      fs.writeFile(`C:\\Users\\ASUS\\Desktop\\BO\\${folderName}/${folderName}-signature.jpg`, signatureBytes, err => {
+        if (err) {
+          console.log(err)
+        }
+      })
       let signature;
       try {
         signature = await pdfDoc.embedPng(signatureBytes); // Try to embed as PNG
@@ -58,15 +191,64 @@ app.post('/modify-pdf', async (req, res) => {
       }
       secondPage.drawImage(signature, {
         x: 400, // Position the image
-        y: secondPageHeight - 649.5, // Position from the top
+        y: secondPageHeight - 645.5, // Position from the top
         width: 102,
-        height: 26
+        height: 19
+      })
+    }
+    if (req.body.clientNidPhoto) {
+      const fileExtension = getFileExtension(req.body.clientNidPhoto)
+      const clientNidPhotoBytes = await fetchImage(req.body.clientNidPhoto)
+
+      fs.writeFile(`C:\\Users\\ASUS\\Desktop\\BO\\${folderName}/${folderName}-client-nid.${fileExtension}`, clientNidPhotoBytes, err => {
+        if (err) {
+          console.log(err)
+        }
+      })
+    }
+    if (req.body.clientNominyPhoto) {
+      const fileExtension = getFileExtension(req.body.clientNominyPhoto)
+
+      const clientNomineePhotoBytes = await fetchImage(req.body.clientNominyPhoto)
+      fs.writeFile(`C:\\Users\\ASUS\\Desktop\\BO\\${folderName}/${folderName}-client-nominee.${fileExtension}`, clientNomineePhotoBytes, err => {
+        if (err) {
+          console.log(err)
+        }
+      })
+    }
+    if (req.body.jointApplicantSign) {
+      const fileExtension = getFileExtension(req.body.jointApplicantSign)
+
+      const jointApplicantSignatureBytes = await fetchImage(req.body.jointApplicantSign)
+      fs.writeFile(`C:\\Users\\ASUS\\Desktop\\BO\\${folderName}/${folderName}-joint-applicant-sign.${fileExtension}`, jointApplicantSignatureBytes, err => {
+        if (err) {
+          console.log(err)
+        }
+      })
+      let jointApplicantSignature;
+      try {
+        jointApplicantSignature = await pdfDoc.embedPng(jointApplicantSignatureBytes); // Try to embed as PNG
+      } catch (error) {
+        jointApplicantSignature = await pdfDoc.embedJpg(jointApplicantSignatureBytes); // Fallback to JPG
+      }
+      secondPage.drawImage(jointApplicantSignature, {
+        x: 400, // Position the image
+        y: secondPageHeight - 669, // Position from the top
+        width: 102,
+        height: 18
       })
     }
 
-
     if (req.body.jointApplicantPhoto) {
+      const fileExtension = getFileExtension(req.body.jointApplicantPhoto)
+
       const jointApplicantPhotoBytes = await fetchImage(req.body.jointApplicantPhoto)
+
+      fs.writeFile(`C:\\Users\\ASUS\\Desktop\\BO\\${folderName}/${folderName}-joint-applicant-photo.${fileExtension}`, jointApplicantPhotoBytes, err => {
+        if (err) {
+          console.log(err)
+        }
+      })
       let jointApplicantPhoto;
       try {
         jointApplicantPhoto = await pdfDoc.embedPng(jointApplicantPhotoBytes); // Try to embed as PNG
@@ -81,22 +263,33 @@ app.post('/modify-pdf', async (req, res) => {
       })
     }
 
+    if (req.body.clientBankDepositeScreenShot) {
+      const fileExtension = getFileExtension(req.body.clientBankDepositeScreenShot)
+
+      const clientBankDepositePhotoBytes = await fetchImage(req.body.clientBankDepositeScreenShot)
+
+      fs.writeFile(`C:\\Users\\ASUS\\Desktop\\BO\\${folderName}/${folderName}-bank-deposite-screenshot.${fileExtension}`, clientBankDepositePhotoBytes, err => {
+        if (err) {
+          console.log(err)
+        }
+      })
+    }
+
+
+
     const fontBytes = fs.readFileSync(__dirname + "/fonts/NotoSansSymbols-Regular.ttf");
-    const bengliFontBytes = fs.readFileSync(__dirname + "/fonts/Shonar.ttf");
+    const bengliFontBytes = fs.readFileSync(__dirname + "/fonts/SutonnyOMJ.ttf");
     const customFont = await pdfDoc.embedFont(fontBytes);
     const customFontBengli = await pdfDoc.embedFont(bengliFontBytes);
     const generateAndPlaceText = (page, type, content, x, y, size, color, spacing, font) => {
-      // Draw each character with spacing
       if (type === 'name' && spacing > 0) {
         for (let i = 0; i < content.length; i++) {
           page.drawText(content[i], {
             x: x,
             y: height - y,
             size: size,
-            font: customFontBengli,
             color: rgb(color.r, color.g, color.b),
           });
-          // Increase x position for the next character
           if (spacing > 0) {
             x += size + spacing;
           } else {
@@ -105,12 +298,10 @@ app.post('/modify-pdf', async (req, res) => {
         }
       } else if (type === 'name' && spacing === 0) {
         page.drawText(content, {
-          // x: 127,   // X position
-          // x: 188,
           x: x,
-          y: height - y,   // Y position
-          size: size, // Adjust size if needed
-          color: rgb(color.r, color.g, color.b), // Green color for the tick mark
+          y: height - y,
+          size: size,
+          color: rgb(color.r, color.g, color.b),
         });
 
       } else if (type !== 'name') {
@@ -132,74 +323,78 @@ app.post('/modify-pdf', async (req, res) => {
     }
 
     // account holder name
-    if(req.body.clientName){
-      generateAndPlaceText(firstPage, 'name', req.body.clientName, 51, 307.8, 10, { r: 0, g: 0, b: 0 }, 4.8, '');
-      generateAndPlaceText(secondPage, 'name', req.body.clientName, 150, 640.8, 12, { r: 0, g: 0, b: 0 }, 0, '');
+    if (req.body.clientName) {
+      generateAndPlaceText(firstPage, 'name', req.body.clientName, 51, 307.8, 10.3, { r: 0, g: 0, b: 0 }, 4.5, '');
+      generateAndPlaceText(secondPage, 'name', req.body.clientName, 125, 640.8, 12, { r: 0, g: 0, b: 0 }, 0, '');
     }
 
-    if(req.body.jointApplicantName){
-      generateAndPlaceText(secondPage, 'name', req.body.jointApplicantName, 150, 665.8, 12, { r: 0, g: 0, b: 0 }, 0, '');
-      generateAndPlaceText(firstPage, 'name', req.body.jointApplicantName, 153, 805.8, 10, { r: 0, g: 0, b: 0 }, 0, '');
+    if (req.body.jointApplicantName) {
+      generateAndPlaceText(secondPage, 'name', req.body.jointApplicantName, 125, 665.8, 12, { r: 0, g: 0, b: 0 }, 0, '');
+      generateAndPlaceText(firstPage, 'name', req.body.jointApplicantName, 150, 805.8, 10, { r: 0, g: 0, b: 0 }, 0, '');
     }
-    
+
 
     // Date of birth
-    if(req.body.clientDateOfBirth){
+    if (req.body.clientDateOfBirth) {
       generateAndPlaceText(firstPage, 'name', req.body.clientDateOfBirth, 445, 664.5, 9, { r: 0, g: 0, b: 0 }, 5.1, '');
     }
 
     // occupation
-    if(req.body.clientOccupation){
+    if (req.body.clientOccupation) {
       generateAndPlaceText(firstPage, 'name', req.body.clientOccupation, 330, 345, 10, { r: 0, g: 0, b: 0 }, undefined, '');
     }
 
     // father/husband name
-    if(req.body.clientGuardian){
+    if (req.body.clientGuardian) {
       generateAndPlaceText(firstPage, 'name', req.body.clientGuardian, 145, 363, 10, { r: 0, g: 0, b: 0 }, undefined, '');
     }
 
     // mother name
-    if(req.body.clientMother){
+    if (req.body.clientMother) {
       generateAndPlaceText(firstPage, 'name', req.body.clientMother, 110, 382, 10, { r: 0, g: 0, b: 0 }, undefined, '');
     }
 
     // address
-    if(req.body.clientAddress){
+    if (req.body.clientAddress) {
       generateAndPlaceText(firstPage, 'name', req.body.clientAddress, 90, 430, 10, { r: 0, g: 0, b: 0 }, 0, '')
     }
 
+    if (req.body.clientDivision) {
+      generateAndPlaceText(firstPage, 'name', req.body.clientDivision, 280, 445, 8, { r: 0, g: 0, b: 0 }, 0, '')
+    }
+
     // city
-    if(req.body.clientCity){
+    if (req.body.clientCity) {
       generateAndPlaceText(firstPage, 'name', req.body.clientCity, 70, 445, 10, { r: 0, g: 0, b: 0 }, 0, '')
     }
 
     // postal code
-    if(req.body.clientPostalCode){
+    if (req.body.clientPostalCode) {
       generateAndPlaceText(firstPage, 'name', req.body.clientPostalCode, 195, 445, 10, { r: 0, g: 0, b: 0 }, 0, '')
     }
 
     // country
-    if(req.body.clientCountry){
+    if (req.body.clientCountry) {
       generateAndPlaceText(firstPage, 'name', req.body.clientCountry, 375, 445, 10, { r: 0, g: 0, b: 0 }, 0, '')
     }
 
     // mobile
-    if(req.body.clientMobileNumber){
-      generateAndPlaceText(firstPage, 'name', req.body.clientMobileNumber, 96, 465, 10, { r: 0, g: 0, b: 0 }, 0, '')
+    if (req.body.clientMobileNumber) {
+      generateAndPlaceText(firstPage, 'name', req.body.clientMobileNumber, 96, 465, 8, { r: 0, g: 0, b: 0 }, 0, '')
     }
 
     // email
-    if(req.body.clientEmail){
-      generateAndPlaceText(firstPage, 'name', req.body.clientEmail, 285, 465, 10, { r: 0, g: 0, b: 0 }, 0, '')
+    if (req.body.clientEmail) {
+      generateAndPlaceText(firstPage, 'name', req.body.clientEmail.slice(7), 285, 465, 10, { r: 0, g: 0, b: 0 }, 0, '')
     }
 
     // nid
-    if(req.body.clientNid){
+    if (req.body.clientNid) {
       generateAndPlaceText(firstPage, 'name', req.body.clientNid, 155, 720, 10, { r: 0, g: 0, b: 0 }, 0, '')
     }
 
     // bank name
-    if(req.body.clientBankName){
+    if (req.body.clientBankName) {
       generateAndPlaceText(firstPage, 'name', req.body.clientBankName, 90, 575, 10, { r: 0, g: 0, b: 0 }, 0, '')
     }
 
@@ -238,6 +433,13 @@ app.post('/modify-pdf', async (req, res) => {
     });
 
     // Send the modified PDF for preview
+    fs.writeFile('pdf.pdf', modifiedPdfBytes, err => {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log('pdf saved!')
+      }
+    })
     res.send(Buffer.from(modifiedPdfBytes));
   } catch (error) {
     console.error('Error modifying PDF:', error);
